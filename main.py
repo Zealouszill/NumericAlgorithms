eee3d701c6d94f4f63f3f5105ff781c0078069a0
S. Stewart <rubix.this@gmail.com>
2019-01-24 21:37:56 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+
+pip install pytest
+
+pytest "filename".py
+"""
+
+
+def AND(a, b):
+    """
+    returns a single character '0'
+    representing a logical AND of
+    bit a and bit b (which are each 0 or 1
+
+    >>> AND('0', '1')
+    '0'
+    >>> AND('1', '1')
+    '1'
+    """
+
+    if (a == '1'):
+        if (b == '1'):
+            return '1'
+        else:
+            return '0'
+    else:
+        return '0'
+
+
+def OR(a, b):
+    """
+    returns a single character '0'
+    representing a logical AND of
+    bit a and bit b (which are each 0 or 1
+
+    >>> AND('0', '1')
+    '0'
+    >>> AND('1', '1')
+    '1'
+    """
+
+    if (a == '1'):
+        return '1'
+
+    else:
+        if (b == '1'):
+            return '1'
+        else:
+            return '0'
+
+
+def XOR(a, b):
+    if (a == '1'):
+        if (b == '1'):
+            return '0'
+        else:
+            return '1'
+    else:
+        if (b == '1'):
+            return '1'
+        else:
+            return '0'
+
+
+def PLUS2(a, b):
+    return a + b
+
+
+def test_and():
+    assert AND('0', '0') == '0'
+    assert AND('0', '1') == '0'
+    assert AND('1', '0') == '0'
+    assert AND('1', '1') == '1'
+
+    assert OR('0', '0') == '0'
+    assert OR('0', '1') == '1'
+    assert OR('1', '0') == '1'
+    assert OR('1', '1') == '1'
+
+    assert XOR('0', '0') == '0'
+    assert XOR('0', '1') == '1'
+    assert XOR('1', '0') == '1'
+    assert XOR('1', '1') == '0'
+
+    assert PLUS2(13, 16) == (29)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
